199f7a20000f78b26beaa573f15a2b9fb326ae04
tam nguyen <ndtam.273@gmail.com>
2020-11-13 17:47:37 +0700

    hihi

---
+++ vc_connect/Controllers/FavoriteVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteVC.swift
+//  vc_connect
+//
+//  Created by TAM NGUYEN DUC on 11/13/20.
+//  Copyright © 2020 TAM NGUYEN DUC. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- vc_connect/Controllers/Home/ProductViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ProductViewModel.swift
-//  vc_connect
-//
-//  Created by TAM NGUYEN DUC on 10/29/20.
-//  Copyright © 2020 TAM NGUYEN DUC. All rights reserved.
-//
-
-import Foundation
